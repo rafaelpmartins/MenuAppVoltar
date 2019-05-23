5d664307e56adbb19c2ffffe816c67acab6c4377
rafaelmartins <Rafael.martins92@etec.sp.gov.br>
2019-05-22 21:12:03 -0300

    Criei seta home e mudei o icone

---
+++ app/src/main/java/br/com/etecia/menuappvoltar/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.etecia.menuappvoltar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.security.Principal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.idToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_dp);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //Toast.makeText(getApplicationContext(), "Clique na seta", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), Principal_Activity.class));
+
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/br/com/etecia/menuappvoltar/Principal_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menuappvoltar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_layout);
+    }
+}
